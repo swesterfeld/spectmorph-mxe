f041e1eb4d737e5f39f05e87c303f728480b0757
Stefan Westerfeld <stefan@space.twc.de>
2023-06-17 16:42:33 +0200

    Fix missing "tar" command in build script.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -Eeuo pipefail
 
 docker build -f Dockerfile -t spectmorph-mxe .
-docker run -v $PWD:/data -t spectmorph-mxe cJvf /data/spectmorph-mxe.tar.xz /mxe/usr
+docker run -v $PWD:/data -t spectmorph-mxe tar cJvf /data/spectmorph-mxe.tar.xz /mxe/usr
