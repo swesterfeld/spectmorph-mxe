acf39351d21cf582307af1234f417479d98e6986
Stefan Westerfeld <stefan@space.twc.de>
2024-06-16 15:07:00 +0200

    Switch from gcc12 to gcc14.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- mxe-settings.mk
+++ mxe-settings.mk
@@ -1,7 +1,7 @@
 ## https://github.com/mxe/mxe.git  ec0b69a6527e3babf30a13d71a150449418c273b
 MXE_TARGETS=x86_64-w64-mingw32.static
-MXE_PLUGIN_DIRS=plugins/gcc12
-JOBS=16
+MXE_PLUGIN_DIRS=plugins/gcc14
+JOBS=32
 
 LOCAL_PKG_LIST := libsndfile glib libao cairo fftw
 .DEFAULT_GOAL  := local-pkg-list
